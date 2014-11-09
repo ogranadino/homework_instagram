2ce09ce599b7ba05c21fb2bc20218d82f0c2097a
Oscar Granadino <ogranadino@gmail.com>
2014-11-08 21:41:09 -0600

    All ready

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,37 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @id = params[:id]
+    z = Photo.find_by({:id => params[:id]})
+    @source = z.source
+    @caption = z.caption
+  end
+
+  def create_row
+    @z = Photo.new
+    @z.caption = params[:the_caption]
+    @z.source = params[:the_source]
+    @z.save
+  end
+
+  def destroy
+    @z = Photo.find(params[:id])
+    @z.destroy
+  end
+
+  def edit_form
+    @id = params[:id]
+    z = Photo.find_by({:id => params[:id]})
+    @source = z.source
+    @caption = z.caption
+  end
+
+  def update_row
+    @z = Photo.find(params[:id])
+    @z.caption = params[:the_caption]
+    @z.source = params[:the_source]
+    @z.save
+  end
 
 end
